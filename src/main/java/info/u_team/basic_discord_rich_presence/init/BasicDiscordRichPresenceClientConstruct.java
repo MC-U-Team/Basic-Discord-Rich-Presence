4fdee3ea9b40f1a83a7f9280334b11d3b8840571
HyCraftHD <hycrafthd@live.de>
2021-03-05 19:38:07 +0100

    Add register stuff

--- src/main/java/info/u_team/basic_discord_rich_presence/init/BasicDiscordRichPresenceClientConstruct.java
+++ src/main/java/info/u_team/basic_discord_rich_presence/init/BasicDiscordRichPresenceClientConstruct.java
@@ -1,9 +1,17 @@
 package info.u_team.basic_discord_rich_presence.init;
 
+import info.u_team.basic_discord_rich_presence.screen.DiscordConfigScreen;
+import info.u_team.u_team_core.intern.discord.UpdateDiscordEventHandler;
+import net.minecraftforge.fml.*;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
+
 public class BasicDiscordRichPresenceClientConstruct {
 	
 	public static void construct() {
+		ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.CONFIGGUIFACTORY, () -> (minecraft, screen) -> new DiscordConfigScreen(screen));
 		
+		UpdateDiscordEventHandler.registerMod(Bus.MOD.bus().get());
+		UpdateDiscordEventHandler.registerForge(Bus.FORGE.bus().get());
 	}
 	
 }
