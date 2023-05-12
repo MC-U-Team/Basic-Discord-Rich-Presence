dff4edf7f71461e46248954d4fab5a20f0ef036e
HyCraftHD <hycrafthd@live.de>
2023-05-12 18:12:58 +0200

    Remove testing

--- fabric/src/main/java/info/u_team/basic_discord_rich_presence/BasicDiscordRichPresenceMod.java
+++ fabric/src/main/java/info/u_team/basic_discord_rich_presence/BasicDiscordRichPresenceMod.java
@@ -1,6 +1,5 @@
 package info.u_team.basic_discord_rich_presence;
 
-import info.u_team.basic_discord_rich_presence.config.ClientConfig;
 import info.u_team.basic_discord_rich_presence.init.BasicDiscordRichPresenceClientConstruct;
 import net.fabricmc.api.ClientModInitializer;
 
@@ -10,8 +9,6 @@ public class BasicDiscordRichPresenceMod implements ClientModInitializer {
 	
 	@Override
 	public void onInitializeClient() {
-		System.out.println(ClientConfig.getInstance().discordRichPresence.get());
-		ClientConfig.getInstance().discordRichPresence.set(!ClientConfig.getInstance().discordRichPresence.get());
 		BasicDiscordRichPresenceClientConstruct.construct();
 	}
 	
